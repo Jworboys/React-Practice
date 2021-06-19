51eba84e964f5bb15fd1dca51c5fce83d0cd5017
jworboys <Jordworb@gmail.com>
2021-06-19 13:04:50 -0400

    Adding starting point to ItemExpenseTracker-React

---
+++ ItemExpenseTracker/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+
+function App() {
+  return (
+    <div>
+      <div>Let's get started</div>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
