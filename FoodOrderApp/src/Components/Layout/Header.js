99aca3a9289ecbe3bebc0f81e8d6c126b82e686c
jworboys <Jordworb@gmail.com>
2021-06-30 13:09:38 -0400

    Adding Css to the cart button in the header

--- FoodOrderApp/src/Components/Layout/Header.js
+++ FoodOrderApp/src/Components/Layout/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import classes from './Header.module.css';
 import mealsImage from '../../Assets/meals.jpg';
+import HeaderCardButton from './HeaderCartButton';
 
 const Header = props => {
 	return (
 		<React.Fragment>
 			<header className={classes.header}>
 				<h1>ReactMeals</h1>
-				<button>Cart</button>
+				<HeaderCardButton />
 			</header>
 			<div className={classes['main-image']}>
 				<img src={mealsImage} alt='A table full of delicious food!'></img>
