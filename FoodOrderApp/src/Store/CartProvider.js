8bfff52d5a369f9c5148054a89d4eabbe61ad396
jworboys <Jordworb@gmail.com>
2021-07-04 13:19:11 -0400

    Making the cart show values based off the state rather then hard codded

--- FoodOrderApp/src/Store/CartProvider.js
+++ FoodOrderApp/src/Store/CartProvider.js
@@ -1,17 +1,51 @@
+import { update } from 'lodash';
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defultCartState = {
+	items: [],
+	totalAmount: 0,
+};
+
+const cartReducer = (state, action) => {
+	if (action.type === 'ADD') {
+		const updatedItems = state.items.concat(action.item);
+		const updatedTotalAmount =
+			state.totalAmount + action.item.price * action.item.amount;
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+
+	return defultCartState;
+};
+
 const CartProvider = props => {
-	const addItemToCartHandler = item => {};
-	const removeItemFromCartHandler = id => {};
+	const [cartState, dispatchCartAction] = useReducer(
+		cartReducer,
+		defultCartState
+	);
+
+	const addItemToCartHandler = item => {
+		dispatchCartAction({ type: 'ADD', item: item });
+	};
+	const removeItemFromCartHandler = id => {
+		dispatchCartAction({ type: 'REMOVE', id: id });
+	};
 
 	const cartContext = {
-		items: [],
-		totalAmount: 0,
+		items: cartState.items,
+		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
 	};
 
-	return <CartContext.Provider>{props.children}</CartContext.Provider>;
+	return (
+		<CartContext.Provider value={cartContext}>
+			{props.children}
+		</CartContext.Provider>
+	);
 };
 
 export default CartProvider;
