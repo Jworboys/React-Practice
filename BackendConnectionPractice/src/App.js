118e5148d7e88a59d39e393401e5c8935a8581f4
jworboys <Jordworb@gmail.com>
2021-07-05 11:31:28 -0400

    Making the requests use async, and allowing the user to see the request is loading when until the data is presented.

--- BackendConnectionPractice/src/App.js
+++ BackendConnectionPractice/src/App.js
@@ -5,25 +5,27 @@ import './App.css';
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
 
-	const fetchMoviesHandler = () => {
+	async function fetchMoviesHandler() {
 		// Default method is GET, otherwise can give a 2nd parameter.
-		fetch('https://swapi.dev/api/films/')
-			.then(response => {
-				return response.json();
-			})
-			.then(data => {
-				const tranformedMovies = data.results.map(movieData => {
-					return {
-						id: movieData.episode_id,
-						title: movieData.title,
-						openingText: movieData.opening_crawl,
-						releaseDate: movieData.release_date,
-					};
-				});
-				setMovies(tranformedMovies);
-			});
-	};
+		// .then is a "promise" it waits for the step prior to be finished.
+		// U can also use await as shown below.
+		setIsLoading(true);
+		const response = await fetch('https://swapi.dev/api/films/');
+		const data = await response.json();
+
+		const tranformedMovies = data.results.map(movieData => {
+			return {
+				id: movieData.episode_id,
+				title: movieData.title,
+				openingText: movieData.opening_crawl,
+				releaseDate: movieData.release_date,
+			};
+		});
+		setMovies(tranformedMovies);
+		setIsLoading(false);
+	}
 
 	return (
 		<React.Fragment>
@@ -31,7 +33,9 @@ function App() {
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
 			<section>
-				<MoviesList movies={movies} />
+				{!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+				{!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+				{isLoading && <p>Loading...</p>}
 			</section>
 		</React.Fragment>
 	);
