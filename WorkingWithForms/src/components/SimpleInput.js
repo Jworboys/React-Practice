a1f6d2b03138d9ec2bfea886925fc2594dd5565c
jworboys <Jordworb@gmail.com>
2021-07-11 19:34:41 -0400

    Updating form to newest version -- about to start working on validation.

--- WorkingWithForms/src/components/SimpleInput.js
+++ WorkingWithForms/src/components/SimpleInput.js
@@ -1,6 +1,7 @@
-import { useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 const SimpleInput = props => {
+	const nameInputRef = useRef();
 	const [enteredName, setEnteredName] = useState('');
 
 	const nameInputChangeHandler = event => {
@@ -9,13 +10,26 @@ const SimpleInput = props => {
 
 	const formSubmissionHandler = event => {
 		event.preventDefault();
+
+		console.log(enteredName);
+		const enteredValue = nameInputRef.current.value;
+		console.log(enteredValue);
+
+		//nameInputRef.current.value = ''; => Not ideal dont manipulate the dom leave that to React.
+		setEnteredName('');
 	};
 
 	return (
-		<form onsubmit={formSubmissionHandler}>
+		<form onSubmit={formSubmissionHandler}>
 			<div className='form-control'>
 				<label htmlFor='name'>Your Name</label>
-				<input type='text' id='name' onInput={nameInputChangeHandler} />
+				<input
+					ref={nameInputRef}
+					type='text'
+					id='name'
+					onInput={nameInputChangeHandler}
+					value={enteredName}
+				/>
 			</div>
 			<div className='form-actions'>
 				<button>Submit</button>
